42bf493bebd148fb277beada74f63aad6a7deb76
SJT00 <saadtaj96@gmail.com>
2021-03-04 22:28:24 -0500

    Starry Night Functional-Push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import "../styles/global.scss"
+import "../styles/starrynight.scss"
+import { Container } from "react-bootstrap"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Container fluid={true}>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <h1>Salam!</h1>
+    </Container>
+  )
 }
