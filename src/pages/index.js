280db2eb81ed7ff5c5fdb72d555f87b880430661
SJT00 <saadtaj96@gmail.com>
2021-03-05 21:55:25 -0500

    Picture Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
-import React from "react"
-import "../styles/global.scss"
-import "../styles/starrynight.scss"
-import { Container } from "react-bootstrap"
+import React from "react";
+import "../styles/global.scss";
+import "../styles/starrynight.scss";
+import { Container } from "react-bootstrap";
+import headShot from "../../static/Headshot.jpg";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div id="stars2"></div>
       <div id="stars3"></div>
       <h1>Salam!</h1>
+      <img className="circle_icon" height="150px" width="150px" src="../../Headshot.jpg" alt="Great Picture of Me"/>
     </Container>
-  )
+  );
 }
