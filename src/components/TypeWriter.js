081601ff585f5dee92e379b0f6e209e4a387aa8d
SJT00 <saadtaj96@gmail.com>
2021-03-26 02:56:18 -0400

    Initial Setup for multi lang welcome effect

--- src/components/TypeWriter.js
+++ src/components/TypeWriter.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "../styles/typewriter.scss";
 export default function TypeWriter(props){
-return(<h1 class="typewriter">{props.text[0]}</h1>)
+    var i = 0;
+    setTimeout(i=1,10000);
+    setTimeout(i=2,14000);
+    setTimeout(i=0,18000);
+    console.log(i);
+    return(<h1 class="typewriter">{props.text[i]}</h1>)
 }
