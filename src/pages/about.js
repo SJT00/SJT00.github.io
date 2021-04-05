e0d8dfac93ca48626daa9874b341100dca00945f
SJT00 <saadtaj96@gmail.com>
2021-04-05 14:34:21 -0400

    pgs and components for other tabs added

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Personal from "../components/personal.js";
+import Layout from "../components/layout.js"
+
+export default function AboutPg() {
+  return (
+    <Layout>
+      <Personal />
+    </Layout>
+  );
+}
---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Work from "../components/work.js";
+import Layout from "../components/layout.js"
+
+export default function ProjectPg() {
+  return (
+    <Layout>
+      <Work />
+    </Layout>
+  );
+}
