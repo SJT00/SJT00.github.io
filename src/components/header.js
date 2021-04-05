d0ffa89c8ba1d9a277708227685981b378f2176c
SJT00 <saadtaj96@gmail.com>
2021-04-05 14:29:17 -0400

    better reflects other functions

--- src/components/header.js
+++ src/components/header.js
@@ -3,25 +3,21 @@ import { Navbar,Nav } from "react-bootstrap";
 import { Helmet } from "react-helmet";
 import "./header.scss";
 
-const Header = () => (
-  <header
-  >
+export default function Header(){
+  return(
+  <header>
     <Helmet>
           <title>{"Saad Taj"}</title>
         </Helmet>
     <Navbar id="navbar" expand="md">
-      {/* <Navbar.Brand>
-        <span>SJT</span>
-      </Navbar.Brand> */}
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link>About</Nav.Link>
-          <Nav.Link>Projects</Nav.Link>
-          <Nav.Link>Hobbies</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/projects">Projects</Nav.Link>
+          <Nav.Link href="/hobbies">Hobbies</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   </header>
-)
-
-export default Header
+  );
+}
