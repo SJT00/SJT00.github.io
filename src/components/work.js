6ad995b0523045df4a75d1ed7a8e04fde837af76
SJT00 <saadtaj96@gmail.com>
2021-04-26 06:38:23 -0400

    Initial work custom carousel

--- src/components/work.js
+++ src/components/work.js
@@ -1,5 +1,30 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import "./work.scss";
 export default function Work() {
-  return(<></>);
+  return (
+    <Row xs={1} id="work">
+      <Col style={{ maxWidth: "800px" }}>
+        <span className="chevron left" />
+        <span className="chevron right" />
+        <img
+          className="d-block w-80 mx-auto"
+          src="../../Caraousel/Budgie.png"
+          alt="First slide"
+        />
+        {/*
+            <ProjCarousel> 
+            <img
+              className="d-block w-100"
+              src="../../Caraousel/Token.png"
+              alt="Second slide"
+            />
+            <img
+              className="d-block w-100"
+              src="../../Caraousel/S_invaders.png"
+              alt="Third slide"
+            /> */}
+      </Col>
+    </Row>
+  );
 }
