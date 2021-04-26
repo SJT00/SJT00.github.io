8149ec4eb7037c74f892f251b46b9463937a679a
SJT00 <saadtaj96@gmail.com>
2021-04-26 09:18:31 -0400

    Carousel init

--- src/components/work.js
+++ src/components/work.js
@@ -1,29 +1,57 @@
-import React from "react";
+import React ,{useState} from "react";
 import { Col, Row } from "react-bootstrap";
 import "./work.scss";
 export default function Work() {
+  const [index, setIndex] = useState(0);
+  const total=3;
   return (
     <Row xs={1} id="work">
       <Col style={{ maxWidth: "800px" }}>
-        <span className="chevron left" />
-        <span className="chevron right" />
-        <img
-          className="d-block w-80 mx-auto"
-          src="../../Caraousel/Budgie.png"
-          alt="First slide"
-        />
-        {/*
-            <ProjCarousel> 
+        <span className="d-block" style={{textAlign:"center"}}>
+          {"Slide: "+(index+1)+"/"+total}
+          </span>
+        <span className="chevron left" onClick={()=>{if(index>0){setIndex(index-1)}}}/>
+        <span className="chevron right" onClick={()=>{if(index<total-1){setIndex(index+1)}}}/>
+        {index === 0 && (
+          <>
             <img
-              className="d-block w-100"
+              className="d-block w-80 mx-auto"
+              src="../../Caraousel/Budgie.png"
+              alt="First slide"
+            />
+            <p classname="imgpar">
+              A Visual Budgeting Application, made using the MERN stack. My
+              first full stack Web-App, made in a Typescript environment.
+            </p>
+          </>
+        )}
+        {index === 1 && (
+          <>
+            <img
+              className="d-block w-80 mx-auto"
               src="../../Caraousel/Token.png"
               alt="Second slide"
             />
+            <p classname="imgpar">
+              A Sobriety Tracking Application I made during Hack the Valley.
+              Made using React, Node and Express.
+            </p>
+          </>
+        )}
+        {index === 2 && (
+          <>
             <img
-              className="d-block w-100"
+              className="d-block w-80 mx-auto"
               src="../../Caraousel/S_invaders.png"
               alt="Third slide"
-            /> */}
+            />
+            <p classname="imgpar">
+              A Space Invader clone I made using python and its library pygame.
+              Can be played with a keyboard, as well as a custom controller.<br/>
+              Sprites and font courtesy of the internet.
+            </p>
+          </>
+        )}
       </Col>
     </Row>
   );
