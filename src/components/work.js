2ba76fe5863d83c4a7e4efeb0e5a8dbd6253bf45
SJT00 <saadtaj96@gmail.com>
2021-04-27 19:45:49 -0400

    Other panels updated appropriately

--- src/components/work.js
+++ src/components/work.js
@@ -17,7 +17,7 @@ export default function Work() {
         </span>
         <span
           className="chevron left"
-          style={{opacity:index===0? "50%":"100%"}}
+          style={{ opacity: index === 0 ? "50%" : "100%" }}
           onClick={() => {
             if (index > 0) {
               setIndex(index - 1);
@@ -26,7 +26,7 @@ export default function Work() {
         />
         <span
           className="chevron right"
-          style={{opacity:index===total-1? "50%":"100%"}}
+          style={{ opacity: index === total - 1 ? "50%" : "100%" }}
           onClick={() => {
             if (index < total - 1) {
               setIndex(index + 1);
@@ -42,7 +42,7 @@ export default function Work() {
               layout="constrained"
               alt="First slide"
             />
-            <p className="imgpar">
+            <div className="imgpar">
               Tech Stack:
               <Row xs={6} sm={8} className="logos">
                 <Col>
@@ -166,9 +166,11 @@ export default function Work() {
                   </OverlayTrigger>
                 </Col>
               </Row>
+              <p>
               A Visual Budgeting Application, made using the MERN stack. My
               first full stack Web-App, made in a Typescript environment.
-            </p>
+              </p>
+            </div>
           </>
         )}
         {index === 1 && (
@@ -180,10 +182,120 @@ export default function Work() {
               layout="constrained"
               alt="Second slide"
             />
-            <p className="imgpar">
+            <div className="imgpar">
+              Tech Stack:
+              <Row xs={5} className="logos">
+                <Col>
+                  <OverlayTrigger
+                    trigger={["hover", "focus"]}
+                    placement="bottom"
+                    overlay={popover("ExpressJS")}
+                  >
+                    <img
+                      src="../../Icons/Languages/expressjs.png"
+                      height="50px"
+                      width="80px"
+                      className="round_icon"
+                      alt="ExpressJS"
+                    />
+                  </OverlayTrigger>
+                </Col>
+                <Col>
+                  <OverlayTrigger
+                    trigger={["hover", "focus"]}
+                    placement="top"
+                    overlay={popover("React")}
+                  >
+                    <img
+                      src="../../Icons/Languages/react.svg"
+                      height="50"
+                      width="70"
+                      className="round_icon"
+                      alt="React"
+                    />
+                  </OverlayTrigger>
+                </Col>
+                <Col>
+                  <OverlayTrigger
+                    trigger={["hover", "focus"]}
+                    placement="top"
+                    overlay={popover("NodeJS")}
+                  >
+                    <img
+                      src="../../Icons/Languages/nodejs.svg"
+                      height="85"
+                      width="85"
+                      className="round_icon"
+                      alt="NodeJS"
+                    />
+                  </OverlayTrigger>
+                </Col>
+                <Col>
+                  <OverlayTrigger
+                    trigger={["hover", "focus"]}
+                    placement="top"
+                    overlay={popover("HTML5")}
+                  >
+                    <img
+                      src="../../Icons/Languages/html5.svg"
+                      height="50"
+                      width="50"
+                      className="round_icon"
+                      alt="HTML5"
+                    />
+                  </OverlayTrigger>
+                </Col>
+                <Col>
+                  <OverlayTrigger
+                    trigger={["hover", "focus"]}
+                    placement="top"
+                    overlay={popover("CSS3")}
+                  >
+                    <img
+                      src="../../Icons/Languages/css3.svg"
+                      height="50"
+                      width="50"
+                      className="round_icon"
+                      alt="CSS3"
+                    />
+                  </OverlayTrigger>
+                </Col>
+                <Col>
+                  <OverlayTrigger
+                    trigger={["hover", "focus"]}
+                    placement="top"
+                    overlay={popover("Javascript")}
+                  >
+                    <img
+                      src="../../Icons/Languages/Jscript.svg"
+                      height="50"
+                      width="50"
+                      className="round_icon"
+                      alt="Javascript"
+                    />
+                  </OverlayTrigger>
+                </Col>
+                <Col>
+                  <OverlayTrigger
+                    trigger={["hover", "focus"]}
+                    placement="right"
+                    overlay={popover("Git")}
+                  >
+                    <img
+                      src="../../Icons/Languages/git.svg"
+                      height={50}
+                      width="50"
+                      className="round_icon"
+                      alt="Git"
+                    />
+                  </OverlayTrigger>
+                </Col>
+              </Row>
+              <p>
               A Sobriety Tracking Application I made during Hack the Valley.
               Made using React, Node and Express.
-            </p>
+              </p>
+            </div>
           </>
         )}
         {index === 2 && (
@@ -195,13 +307,63 @@ export default function Work() {
               layout="constrained"
               alt="Third slide"
             />
-            <p className="imgpar">
+            <div className="imgpar">
+              Tech Stack:
+              <Row xs={3} className="logos">
+                <Col>
+                  <OverlayTrigger
+                    trigger={["hover", "focus"]}
+                    placement="left"
+                    overlay={popover("Python")}
+                  >
+                    <img
+                      src="../../Icons/Languages/python.svg"
+                      height="50"
+                      width="50"
+                      className="round_icon"
+                      alt="Python"
+                    />
+                  </OverlayTrigger>
+                </Col>
+                <Col>
+                  <OverlayTrigger
+                    trigger={["hover", "focus"]}
+                    placement="bottom"
+                    overlay={popover("Pygame")}
+                  >
+                    <img
+                      src="../../Icons/Languages/pygame.gif"
+                      height="100px"
+                      width="100px"
+                      className="round_icon"
+                      alt="Pygame"
+                    />
+                  </OverlayTrigger>
+                </Col>
+                <Col>
+                  <OverlayTrigger
+                    trigger={["hover", "focus"]}
+                    placement="right"
+                    overlay={popover("Git")}
+                  >
+                    <img
+                      src="../../Icons/Languages/git.svg"
+                      height={50}
+                      width="50"
+                      className="round_icon"
+                      alt="Git"
+                    />
+                  </OverlayTrigger>
+                </Col>
+              </Row>
+              <p>
               A Space Invader clone I made using python and its library pygame.
               Can be played with a keyboard, as well as a custom controller.
               <br />
               <br />
               Sprites and font courtesy of the internet.
-            </p>
+              </p>
+            </div>
           </>
         )}
       </Col>
