c35efa98f18c6f94b76d82e0aeb4ff015c228480
SJT00 <saadtaj96@gmail.com>
2021-04-28 21:48:57 -0400

    no longer needed

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Intro from "../components/intro.js";
 import Layout from "../components/layout.js";
 
 export default function Home() {
-  const testing = false;//Always set this to false before hosting
   return (
     <Layout>
       <Intro />
