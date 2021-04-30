3686dd225e9375048c679e966e6bcfb1815b132b
SJT00 <saadtaj96@gmail.com>
2021-04-29 23:31:44 -0400

    Consistent mobile res

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,22 @@ import Background from "../components/bkgd.js";
 import Header from "./header";
 import "./layout.scss";
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 const Layout = ({ children }) => {
   return (
     <>
-      <Container fluid style={{ height: "100vh" }}>
+      <Container className="layoutContainer" fluid>
         <Header />
         <Background />
         <main>{children}</main>
