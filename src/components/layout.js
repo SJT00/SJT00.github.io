b9eb66368a6b8faa000700389aee8405dfdddf9d
SJT00 <saadtaj96@gmail.com>
2021-04-29 23:35:24 -0400

    avoid window error

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,17 +7,17 @@ import Background from "../components/bkgd.js";
 import Header from "./header";
 import "./layout.scss";
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// Code Excerpt from https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+// Avoids addressbar scrolling on mobile
+if (typeof window !== "undefined") {
 let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
+document.documentElement.style.setProperty('--vh', `${vh}px`);//Use custom vh setting for styling
 window.addEventListener('resize', () => {
-  // We execute the same script as before
+  // React to resizing
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+}
 
 const Layout = ({ children }) => {
   return (
