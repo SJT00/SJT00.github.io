41e0d9aadc27f1b46c754802c0d5eae585a18d7c
SJT00 <saadtaj96@gmail.com>
2021-05-04 22:17:41 -0400

    continued formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: "/",
   siteMetadata: {
     title: `Saad Taj`,
-    siteUrl: 'https://sjt00.github.io/',
+    siteUrl: "https://sjt00.github.io/",
     description: `Software Developer, Frontend Designer and Honours Mathematics Student`,
     author: `SJT00`,
   },
@@ -12,16 +12,16 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     {
-      resolve:'gatsby-plugin-manifest',
-      options:{
-        icon:'static/Logo.png',
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "static/Logo.png",
         name: `GatsbyJS`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-      }
-    }
+      },
+    },
   ],
-}
+};
