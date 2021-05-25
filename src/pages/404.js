85622f59059794801976af344275efe81de4ec35
SJT00 <saadtaj96@gmail.com>
2021-05-24 23:02:22 -0400

    Fix for overwritten layout on 404pg

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,30 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import { Container } from "react-bootstrap";
+import Layout from "../components/layout.js";
 
 export default function NotFound() {
   //Temp layout seems to bug out other pages
   return (
-    <>
-      <Container fluid style={{ height: "100vh" }}>
-        <Row
-          style={{
-            height: "60vh",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Col style={{ textAlign: "center" }}>
-            <h3>
-              What the!!! how did you get here?
-              <br />
-              <br />
-              You know where the exit is...
-            </h3>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Layout>
+      {/* <Container fluid style={{ height: "100vh" }}> */}
+      <Row
+        style={{
+          height: "60vh",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Col style={{ textAlign: "center" }}>
+          <h3>
+            What the!!! how did you get here?
+            <br />
+            <br />
+            You know where the exit is... ↗️
+          </h3>
+        </Col>
+      </Row>
+      {/* </Container> */}
+    </Layout>
   );
 }
