c12d857da5b4ad862a0c29282bf0b0922fa66908
SJT00 <saadtaj96@gmail.com>
2021-07-09 15:41:25 -0400

    Added analytics and workflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,5 +23,16 @@ module.exports = {
         display: `standalone`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "GA-TRACKING_ID", // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
   ],
 };
