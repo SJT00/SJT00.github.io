322392408b1e5ca6490498b3488d03a851fe0503
SJT00 <saadtaj96@gmail.com>
2023-03-13 23:25:14 -0400

    Google Analytics checking

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,17 @@ module.exports = {
     author: `SJT00`,
   },
   plugins: [
+    {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      trackingIds: [
+        "G-KEMH0Y8HS6", // Google Analytics / GA
+      ],
+      pluginConfig: {
+        head: true,
+      },
+    },
+  },
     "gatsby-plugin-sass",
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
@@ -19,17 +30,6 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "GA-TRACKING_ID", // Google Analytics / GA
-          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
       },
     },
   ],
