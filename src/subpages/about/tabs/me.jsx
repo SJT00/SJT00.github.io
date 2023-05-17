dc5200659bfadf5847633ee93ffb66c5cf9ac509
SJT00 <saadtaj96@gmail.com>
2023-05-17 15:12:47 -0400

    formatting

--- src/subpages/about/tabs/me.jsx
+++ src/subpages/about/tabs/me.jsx
@@ -9,21 +9,21 @@ export default function me() {
         <Chart />
       </Row>
       <Row>
-      <p>
-        I am an Honours Mathematics graduate from the University of Waterloo,
-        with a passion for (and experience in) Computer Science.
-        <br />
-        <br />
-        I love creating interactive applications, whether they be websites,
-        games, organizational tools, or just fun projects to learn new
-        technologies.
-        <br />
-        See more about my hobbies in the above tabs, but as a primer...
-        <br />
-        <br />
-        I'm a history buff, tech tinkerer, fitness devotee, music addict,
-        struggling reader, story telling enthusiast, and horrible cook.
-      </p>
+        <p>
+          I am an Honours Mathematics graduate from the University of Waterloo,
+          with a passion for (and experience in) Computer Science.
+          <br />
+          <br />
+          I love creating interactive applications, whether they be websites,
+          games, organizational tools, or just fun projects to learn new
+          technologies.
+          <br />
+          See more about my hobbies in the above tabs, but as a primer...
+          <br />
+          <br />
+          I'm a history buff, tech tinkerer, fitness devotee, music addict,
+          struggling reader, story telling enthusiast, and horrible cook.
+        </p>
       </Row>
     </Col>
   );
