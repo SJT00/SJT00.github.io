2a3dbcecda1df0c8a77c99123771f252a9f277cb
SJT00 <saadtaj96@gmail.com>
2023-05-17 15:12:28 -0400

    revised history section

--- src/subpages/about/tabs/history.jsx
+++ src/subpages/about/tabs/history.jsx
@@ -3,28 +3,31 @@ import React from "react";
 export default function history() {
   return (
     <p>
-      I love all things History! from the start of human civilization to
-      contemporary geopolitics, History's intersection with creativity and
-      storytelling, and of course, its' relevance to modernity, fascinate me.
+      <h4>My Favourite 'Past-time'</h4>
+      <span className="quote">
+        History doesn't repeat itself, but it does often rhymes
+      </span>
+      <span className="author">(Mark Twain)</span>
+      &nbsp; rings true to me, the almost eerie relatability of our ancestors-
+      despite all the progress dividing us- fascinates me!
       <br />
       <br />
-      Mark Twain's observation
-      <span>"History doesn't repeat itself but it often rhymes"</span> rings
-      true to me, the almost eerie relatability of our ancestors- despite all
-      the "progress" dividing us- is quite astonishing.
+      <h4>
+        Previously<em>...</em>
+      </h4>
+      I have explored Antiquity, The Islamic Golden Age, Medieval Eurasia, The
+      Age Of Discovery, The Gunpowder Empires, The Age of Imperialism, and
+      Decolonization.
       <br />
       <br />
-      Periods I have explored in the past include Antiquity, The Islamic Golden
-      Age, Medieval Eurasia, The Age Of Discovery, The Gunpowder Empires, The
-      Age of Imperialism, and Decolonization.
+      <h4>Current Fascination</h4>
+      These days I'm preoccupied with the early modern period, in which the
+      beginnings of true globalization, the fundamental foundation of the modern
+      world (particularly regarding World-Systems Theory), and the infancy of
+      current political paradigms began.
       <br />
       <br />
-      Currently, I'm preoccupied with the early modern period, the beginnings of
-      true globalization in this period, the fundamental foundations of the
-      modern world laid out (particularly regarding World-systems theory), and
-      the infancy of current archetypes.
-      <br />
-      <br />
+      <h4>Next In Line</h4>
       In the next few years, I hope to tackle the Victorian age, a much more
       recognizable world, which's consequences are much more easily translatable
       to current events.
