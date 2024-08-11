fb3be3fd29f15ad88d3c2d80ac9123c86f444afe
SJT00 <saadtaj96@gmail.com>
2024-08-11 07:38:44 -0400

    fixed projects layout

--- src/subpages/project/projectsSubpg.jsx
+++ src/subpages/project/projectsSubpg.jsx
@@ -10,20 +10,35 @@ import WorkingOn from "./tabs/workingOn";
 
 export default function Work() {
   return (
-    <Row id="work">
-      <Row>
-        <Col style={{ maxWidth: "900px", minHeight: "600px" }}>
-          <WorkingOn />
-        </Col>
-      </Row>
-      <Row>
-        <CardGroup>
-          <Budgie />
-          <Token />
-          <SpaceInvaders />
-          <FlashFightingGame />
-        </CardGroup>
-      </Row>
+    <Row xs={1} id="work">
+      <Col style={{ maxWidth: "900px", minHeight: "600px" }}>
+        <Row
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Col xs={10}>
+            <WorkingOn />
+          </Col>
+        </Row>
+        <Row
+          className="previousProjects"
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Col>
+            <CardGroup>
+              <Budgie />
+              <Token />
+              <SpaceInvaders />
+              <FlashFightingGame />
+            </CardGroup>
+          </Col>
+        </Row>
+      </Col>
     </Row>
   );
 }
--- src/subpages/project/tabs/workingOn.jsx
+++ src/subpages/project/tabs/workingOn.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import CustomGitCalendar from "./components/CustomGitCalendar";
 import justStartDate from "./components/justStartDate";
 import { toZonedTime } from "date-fns-tz";
@@ -31,13 +32,7 @@ export default () => {
   };
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
+      <Row>
         <h4
           style={{
             border: "3px solid #d6d4d3",
@@ -47,6 +42,12 @@ export default () => {
         >
           Currently working on:{" "}
         </h4>
+      </Row>
+      <Row
+      style={{
+        justifyContent: "flex-end",
+      }}
+      >
         <span
           style={{
             fontSize: "75%",
@@ -55,30 +56,33 @@ export default () => {
         >
           Last updated on {datesInStyle(lastUpdated)}
         </span>
-      </div>
-      <p>
-        I am embarking on a journey to do some challenging CS projects based of
-        this{" "}
-        <a href="https://austinhenley.com/blog/challengingprojects.html">
-          blog post
-        </a>{" "}
-        to help my lower-level understanding and intuition.
-      </p>
-      <p>
-        I dedicate this segment to keeping myself accountable while committing
-        to <b>3-4 Github contributions a week</b> (Starting&nbsp;
-        <span
-          style={{
-            color: "#FFFF00",
-          }}
-        >
-          {datesInStyle(justStartDate)}
-        </span>
-        ), here's my Git calendar, lets see how I do!
-      </p>
-
-      <CustomGitCalendar startDate={startDate} />
-      <div>
+      </Row>
+      <Row>
+        <p>
+          I am embarking on a journey to do some challenging CS projects based
+          of this{" "}
+          <a href="https://austinhenley.com/blog/challengingprojects.html">
+            blog post
+          </a>{" "}
+          to help my lower-level understanding and intuition.
+        </p>
+        <p>
+          I dedicate this segment to keeping myself accountable while committing
+          to <b>3-4 Github contributions a week</b> (Starting&nbsp;
+          <span
+            style={{
+              color: "#FFFF00",
+            }}
+          >
+            {datesInStyle(justStartDate)}
+          </span>
+          ), here's my Git calendar, lets see how I do!
+        </p>
+      </Row>
+      <Row>
+        <CustomGitCalendar startDate={startDate} />
+      </Row>
+      <Row>
         <h5> Projects Pipeline</h5>
         <ol>
           <li>Making a Vim Style Text Editor with C++</li>
@@ -86,30 +90,8 @@ export default () => {
           <li style={blurStyle}>Building own language</li>
           <li style={blurStyle}>Building own OS</li>
         </ol>
-      </div>
-      <p>Check out some past projects below!</p>
-      <span
-        style={{
-          position: "absolute",
-          transform: "rotate(-12deg)",
-          right: 0,
-          top: "66vh",
-          color: "#EE4B2B",
-          fontSize: "2rem",
-          fontWeight: "500",
-          border: "0.25rem solid #EE4B2B",
-          display: "inline-block",
-          padding: "0.25rem 1rem",
-          textTransform: "uppercase",
-          borderRadius: "1rem",
-          fontFamily: "Courier",
-          webkitMaskImage:
-            "url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/8399/grunge.png')",
-          zIndex: "100",
-        }}
-      >
-        Under Construction
-      </span>
+      </Row>
+      <p>Check out some of my past projects below!</p>
     </>
   );
 };
