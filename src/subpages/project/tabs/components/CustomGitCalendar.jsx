5093c10bd632030bbd4eea70d149d806c91a525b
SJT00 <saadtaj96@gmail.com>
2024-08-10 20:49:12 -0400

    fully working git calendar

--- src/subpages/project/tabs/components/CustomGitCalendar.jsx
+++ src/subpages/project/tabs/components/CustomGitCalendar.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
+import { format, toZonedTime } from "date-fns-tz";
 import GitHubCalendar from "react-github-calendar";
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
@@ -6,12 +7,15 @@ import "tippy.js/dist/tippy.css";
 export default (
   { startDate } // Add startDate prop
 ) => {
-  const formattedStartDate = new Date(startDate).toISOString().slice(0, 10);
-  const today = new Date().toISOString().slice(0, 10);
-  const Wrapper = ({ color, children }) => {
-    if (!color) return <>{children}</>;
-    return <>{children}</>;
-  };
+  const formattedStartDate = format(
+    toZonedTime(new Date(startDate), "America/New_York"),
+    "yyyy-MM-dd"
+  );
+  const today = format(
+    toZonedTime(new Date(), "America/New_York"),
+    "yyyy-MM-dd"
+  );
+  // const today = utcDate.slice(0, 10);
   return (
     <GitHubCalendar
       username="SJT00"
@@ -26,6 +30,7 @@ export default (
       renderBlock={(b, a) => {
         let dayLabel = "";
         let color = "";
+        // console.log(a.date, today);
         if (a.date === today) {
           dayLabel = "Today";
           color = "#ff0000";
@@ -33,27 +38,33 @@ export default (
           dayLabel = "Start Date";
           color = "#FFFF00";
         } else {
-          dayLabel = new Date(a.date).toLocaleDateString("en-US", {
+          dayLabel = toZonedTime(
+            new Date(a.date),
+            "America/New_York"
+          ).toLocaleDateString("en-US", {
             month: "long",
             day: "numeric",
             timeZone: "UTC",
           });
         }
         const content = dayLabel + `: ${a.count} contributions`;
+        const style = {
+          stroke: color,
+          strokeWidth: "2px",
+        };
+        const c = React.cloneElement(b, { style });
         return (
-          <Wrapper color={color}>
-            <Tippy
-              content={content}
-              placement="top"
-              theme="translucent"
-              animation="fade"
-              duration={[0, 200]}
-              inertia={true}
-              arrow={true}
-            >
-              {b}
-            </Tippy>
-          </Wrapper>
+          <Tippy
+            content={content}
+            placement="top"
+            theme="translucent"
+            animation="fade"
+            duration={[0, 200]}
+            inertia={true}
+            arrow={true}
+          >
+            {c}
+          </Tippy>
         );
       }}
     />
--- src/subpages/project/tabs/workingOn.jsx
+++ src/subpages/project/tabs/workingOn.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Form } from "react-bootstrap";
 import CustomGitCalendar from "./components/CustomGitCalendar";
 import justStartDate from "./components/justStartDate";
+import { toZonedTime } from "date-fns-tz";
 
 export default () => {
   const blurStyle = {
     filter: "blur(6px)",
     userSelect: "none",
   };
-  const startDate = justStartDate;
-  const lastUpdated = new Date("2024-08-09");
+  // Add Day for UTC to EST conversion
+  const startDate = new Date(justStartDate); // Used for Git Calendar
+  startDate.setDate(startDate.getDate() + 1); // Add days
+  const lastUpdated = toZonedTime(new Date("2024-08-09"), "America/New_York");
   const datesInStyle = date => {
     return (
       <span
@@ -37,12 +39,14 @@ export default () => {
         }}
       >
         <h4
-        style={{
-          border: "3px solid #d6d4d3",
-          padding: "10px",
-          borderRadius: "10px",
-        }}
-        >Currently working on: </h4>
+          style={{
+            border: "3px solid #d6d4d3",
+            padding: "10px",
+            borderRadius: "10px",
+          }}
+        >
+          Currently working on:{" "}
+        </h4>
         <span
           style={{
             fontSize: "75%",
@@ -68,16 +72,16 @@ export default () => {
             color: "#FFFF00",
           }}
         >
-          {datesInStyle(startDate)}
+          {datesInStyle(justStartDate)}
         </span>
-        ), Here is my Git calendar, lets see how I do!
+        ), here's my Git calendar, lets see how I do!
       </p>
 
       <CustomGitCalendar startDate={startDate} />
       <div>
         <h5> Projects Pipeline</h5>
         <ol>
-          <li>Making a Vim Style Text Editor with C</li>
+          <li>Making a Vim Style Text Editor with C++</li>
           <li style={blurStyle}>Making a Doom Style 3D Environment</li>
           <li style={blurStyle}>Building own language</li>
           <li style={blurStyle}>Building own OS</li>
