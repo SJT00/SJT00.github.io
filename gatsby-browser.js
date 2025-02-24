9ce0765c7f3433199cfc6e02469294e47fb64ee6
SJT00 <saadtaj96@gmail.com>
2025-02-24 15:41:14 -0500

    prettier

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,7 @@
 export const onRouteUpdate = ({ location }) => {
-    if (typeof window !== "undefined" && window.gtag) {
-      window.gtag("config", "G-KEMH0Y8HS6", {
-        page_path: location.pathname,
-      });
-    }
-  };
-  
+  if (typeof window !== "undefined" && window.gtag) {
+    window.gtag("config", "G-KEMH0Y8HS6", {
+      page_path: location.pathname,
+    });
+  }
+};
