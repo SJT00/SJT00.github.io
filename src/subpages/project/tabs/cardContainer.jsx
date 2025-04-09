bf2f30aa068b4932d6a2d73d77c9821acb3a0eaa
SJT00 <saadtaj96@gmail.com>
2025-04-09 10:32:53 -0400

    added card swiping

--- src/subpages/project/tabs/cardContainer.jsx
+++ src/subpages/project/tabs/cardContainer.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { CardGroup } from "react-bootstrap";
 import ShowcaseCard from "../cards/showcaseCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import { EffectCards } from "swiper/modules";
 
 const CardContainer = ({ currentViewing }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <CardGroup>
-      {currentViewing.map(cardInfo => (
-        <ShowcaseCard card={cardInfo} />
-      ))}
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+        cardsEffect={{ slideShadows: false }}
+        onSlideChange={swiper => setActiveIndex(swiper.activeIndex)}
+        touchRatio={1.5}
+        speed={600}
+        style={{
+          overflow: "hidden",
+        }}
+      >
+        {currentViewing.map((cardInfo, idx) => {
+          const isVisible = Math.abs(idx - activeIndex) <= 3;
+          return (
+            <SwiperSlide
+              key={"slider :" + idx}
+              style={{
+                opacity: isVisible ? 1 : 0,
+                pointerEvents: isVisible ? "auto" : "none",
+                transition: "opacity 0.3s ease",
+                width: "fit-content",
+                height: "fit-content",
+              }}
+            >
+              <ShowcaseCard card={cardInfo} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </CardGroup>
   );
 };
