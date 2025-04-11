5c0dc540861961edd83e9056d2f2dd32133de834
SJT00 <saadtaj96@gmail.com>
2025-04-10 23:17:35 -0400

    not needed perhaps?

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-export const onRouteUpdate = ({ location }) => {
-  if (typeof window !== "undefined" && window.gtag) {
-    window.gtag("config", "G-KEMH0Y8HS6", {
-      page_path: location.pathname,
-    });
-  }
-};
